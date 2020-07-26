bf1e70e2386ae7acd8f51eeaa20ae123fff32fcc
vinodrajendran001 <vinodr.msec@gmail.com>
2020-07-26 22:32:00 +0800

    Traditional ML completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine learning to analyze, model and classify time-series and signals',
+    author='vinod',
+    license='',
+)
